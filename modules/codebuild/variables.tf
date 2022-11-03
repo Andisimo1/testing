fc8f5fcd2651967e440227dbd09f1b1c816472b3
Andisimo1 <112399592+Andisimo1@users.noreply.github.com>
2022-11-03 10:54:54 +0200

    Delete modules directory

--- modules/codebuild/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {}
-variable "private_subnet" {}
-variable "github_token" {
-    default = ""
-}
-variable "region" {}
-variable "buildspec" {
-  default = "project/config/buildspec.yaml"
-}
--- modules/ecs/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "vpc_id" {}
-variable "private_subnet" {}
-variable "alb_listener" {}
-variable "iam_role" {}
